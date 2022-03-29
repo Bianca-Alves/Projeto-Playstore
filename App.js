f406c71143cdcc077b0c89cc30be1e8516b586a0
Bianca-Alves <biasantosalves04@gmail.com>
2022-03-28 23:16:39 -0300

    criando front-end e adicionando botões

---
+++ App.js
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import React from 'react'
+import Buttons from "./src/components/Buttons";
+import Header from "./src/components/Header";
+import './App.css'
+
+export default function App() {
+  return (
+    <View style={{backgroundColor:'#D3D3D3', flex:1}}>
+      <Header />
+      <Buttons 
+        button1={{logo: 'game-controller', text: 'sobe', color: '#0CABA8'}} 
+        button2={{logo: 'game-controller', text: 'desce', color: '#008F8C'}} 
+      />
+    </View>
+  );
+}
