f2e2b9b71332bee3a1cad571608f438b6f8fb519
Bianca-Alves <biasantosalves04@gmail.com>
2022-04-26 19:35:56 -0300

    updated files

---
+++ src/components/Header/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import { Ionicons} from '@expo/vector-icons';
+
+
+export default function header(){
+    return(
+        
+        <View style={styles.header}>
+            <Ionicons name="menu" size={24} color="white" />
+            <Text style ={styles.text}> Play store </Text>
+        
+            <Ionicons name="ios-search-sharp" size={24} color="white" />
+        </View>
+
+    );
+}
+const styles = StyleSheet.create({
+    header:{
+        backgroundColor:'#002333',
+<<<<<<< HEAD
+        marginTop:0,
+        width:'100vw',
+=======
+        marginTop:8,
+        width:'auto',
+>>>>>>> 5885db4 (updated files)
+        padding:20,
+        margin:2,
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginBottom:20
+
+    },
+
+    text:{
+        color:'white',
+        marginLeft:-120,
+        fontSize:20,
+        fontWeight:'bold'
+    }
+
+})
